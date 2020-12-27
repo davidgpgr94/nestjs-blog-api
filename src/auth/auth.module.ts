ab342fca88320071e9161325ad93aeeb32e13c16
davidgpgr94 <davidgpgr94@gmail.com>
2020-12-27 19:23:27 +0100

    Implements passport local-strategy.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,14 +1,21 @@
 import { Module } from '@nestjs/common';
 
+import { PassportModule } from '@nestjs/passport';
+
 import { AuthService } from '@Auth/services/auth.service';
+import { LocalStrategy } from '@Auth/strategies/local.strategy';
 import { UsersModule } from '@Users/users.module';
 import { BcryptService } from '@Common/utils/bcrypt.service';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    PassportModule
+  ],
   providers: [
     BcryptService,
     AuthService,
+    LocalStrategy
   ]
 })
 export class AuthModule {}
---
+++ src/auth/strategies/local.strategy.ts
@@ -0,0 +1,26 @@
+
+import { Strategy } from 'passport-local';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+
+import { AuthService } from '@Auth/services/auth.service';
+
+@Injectable()
+export class LocalStrategy extends PassportStrategy(Strategy) {
+
+  constructor(private authService: AuthService) {
+    super({
+      usernameField: 'login',
+      passwordField: 'password'
+    });
+  }
+
+  async validate(username: string, password: string): Promise<any> {
+    const user = await this.authService.validateUser(username, password);
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+    return user;
+  }
+
+}
