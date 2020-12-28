c8c8b8a53aa5010b3b9ca90df4cb610f80479acb
davidgpgr94 <davidgpgr94@gmail.com>
2020-12-28 17:08:54 +0100

    Create auxiliar Public decorator.

--- src/auth/guards/jwt-auth.guard.ts
+++ src/auth/guards/jwt-auth.guard.ts
@@ -1,6 +1,25 @@
 
-import { Injectable } from '@nestjs/common';
+import { ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
 
+import { IS_PUBLIC_KEY } from '@Auth/decorators/public.decorator';
+
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  constructor(private reflector: Reflector) {
+    super();
+  }
+
+  canActivate(ctx: ExecutionContext) {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      ctx.getHandler(),
+      ctx.getClass()
+    ]);
+    if (isPublic) {
+      return true;
+    } else {
+      return super.canActivate(ctx);
+    }
+  }
+}
